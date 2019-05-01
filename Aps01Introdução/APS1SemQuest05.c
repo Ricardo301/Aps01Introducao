0fbb411e784137435b6db4be3debb5f4111f7f1c
Ricardo301 <33549097+Ricardo301@users.noreply.github.com>
2019-05-01 15:58:25 -0300

    ATT 01/05

--- Aps01Introdução/APS1SemQuest05.c
+++ Aps01Introdução/APS1SemQuest05.c
@@ -3,15 +3,23 @@
 
 
 int main(){
-	int num,j;
+	int num,j,i;
 
 	printf("Numero: ");
 	scanf("%d",&num);
 	j=num;
 	for(num;num>=1; num--){
 		printf("%d ",num);
+		
+		
 	
 }
+printf("\n");
+     for(i=1;j>i;i++){
+     	printf("%d \n",j);
+     	
+     	
+	 }
 
 	return 0;
 }
