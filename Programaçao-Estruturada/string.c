0ae06b460b2a27a8eda9407baa2b0bef910cf745
RICARDO SANTOS <2019101306@UC.ACAD>
2019-09-10 22:19:01 -0300

    10/09

--- Programaçao-Estruturada/string.c
+++ Programaçao-Estruturada/string.c
@@ -11,16 +11,19 @@ int main()
     setlocale (LC_ALL, "");
     
 printf("DÍ o valor da string x\n");
-scanf("%s", x);
+gets(x);
+//scanf("%s", x);
 
  
 strcpy(y,"roberto");
-z=strcmp(x,y);
+z=strncmp(x,y,2);
 if (z==0) 
 printf ("As strings s√o iguais\n");
 else
 printf ("As strings s√o diferentes\n");
 
 printf ("Concatenando fica %s",strcat(x,y));
+
+return 0;
 
 }
